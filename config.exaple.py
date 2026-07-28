931a606373b3cc7f3321f63f027736a29e66a6cd
livdev <livdev@users.noreply.github.com>
2026-07-28 14:37:51 +0200

    updating head placemnet

--- config.exaple.py
+++ config.exaple.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 # Update this path to the directory containing your EDF recordings
 DATA_FOLDER = Path("/path/to/edf_files")
+DATA_FOLDER2 = Path("/path/to/electrode/placement")
